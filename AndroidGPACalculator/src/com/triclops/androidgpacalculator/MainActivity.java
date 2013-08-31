714fdb26847e47802f1fe4384de5b1b3adb1eea0
Connor Belanger <connor.belanger@gmail.com>
2013-08-31 16:51:50 -0700

    Version 0.01 -Uploading partial layout ---- Using to change to Mac.

--- AndroidGPACalculator/src/com/triclops/androidgpacalculator/MainActivity.java
+++ AndroidGPACalculator/src/com/triclops/androidgpacalculator/MainActivity.java
@@ -3,13 +3,23 @@ package com.triclops.androidgpacalculator;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
+	
+	private Spinner class1Spin, class2Spin;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		class1Spin = (Spinner) findViewById(R.id.class1_spinner);
+		class2Spin = (Spinner) findViewById(R.id.class2_spinner);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gradeArray, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		class1Spin.setAdapter(adapter);
+		class2Spin.setAdapter(adapter);
 	}
 
 	@Override
