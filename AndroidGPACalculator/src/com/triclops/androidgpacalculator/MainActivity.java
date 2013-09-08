ed65fec02be5937dc8c00f19508c0a97d2c94703
Connor Belanger <connor.belanger@gmail.com>
2013-09-08 15:59:30 -0700

    Version 0.065
    Changed spinners to get entries from XML. Should improve load
    performance.

--- AndroidGPACalculator/src/com/triclops/androidgpacalculator/MainActivity.java
+++ AndroidGPACalculator/src/com/triclops/androidgpacalculator/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -22,12 +21,6 @@ public class MainActivity extends FragmentActivity {
 		class2Spin = (Spinner) findViewById(R.id.class2_spinner);
 		class3Spin = (Spinner) findViewById(R.id.class3_spinner);
 		class4Spin = (Spinner) findViewById(R.id.class4_spinner);
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gradeArray, android.R.layout.simple_spinner_item);
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		class1Spin.setAdapter(adapter);
-		class2Spin.setAdapter(adapter);
-		class3Spin.setAdapter(adapter);
-		class4Spin.setAdapter(adapter);
 	}
 
 	@Override
