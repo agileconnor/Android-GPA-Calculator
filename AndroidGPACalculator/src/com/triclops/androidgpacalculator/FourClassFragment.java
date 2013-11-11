7c757c67d90411d3b66d720da381ed17ee44375f
Connor Belanger <connor.belanger@gmail.com>
2013-11-11 15:02:10 -0700

    Version 0.061 ---- -Added button per fragment to action bar. -Weird nullpointerexception occuring preventing app from launching.

--- AndroidGPACalculator/src/com/triclops/androidgpacalculator/FourClassFragment.java
+++ AndroidGPACalculator/src/com/triclops/androidgpacalculator/FourClassFragment.java
@@ -5,6 +5,9 @@ import com.triclops.androidgpacalculator.R;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -12,6 +15,23 @@ public class FourClassFragment extends Fragment{
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.fragment_4classlayout, container, false);
+	}
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+	}
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.add, menu);
+	}
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.add_button:
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);	
+		}
 	}
 
 }
