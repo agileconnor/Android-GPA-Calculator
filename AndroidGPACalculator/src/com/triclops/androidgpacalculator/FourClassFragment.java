cf07aa7ab5fc4c5d426ee31f3bb75c321dbc80bf
Connor Belanger <connor.belanger@gmail.com>
2013-11-12 21:16:28 -0700

    Version 0.062 ---- -Fixed null pointer exception that was caused by spinners. -Moved spinners to fragment control -Updated icons -Button no longer works

--- AndroidGPACalculator/src/com/triclops/androidgpacalculator/FourClassFragment.java
+++ AndroidGPACalculator/src/com/triclops/androidgpacalculator/FourClassFragment.java
@@ -10,18 +10,34 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FourClassFragment extends Fragment{
-	@Override
+	
+	private Spinner class1Spin, class2Spin, class3Spin, class4Spin;
+	private TextView gpaLabel;
+	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_4classlayout, container, false);
+		View fragView = inflater.inflate(R.layout.fragment_4classlayout, container, false);
+		class1Spin = (Spinner) fragView.findViewById(R.id.class1_spinner);
+		class2Spin = (Spinner) fragView.findViewById(R.id.class2_spinner);
+		class3Spin = (Spinner) fragView.findViewById(R.id.class3_spinner);
+		class4Spin = (Spinner) fragView.findViewById(R.id.class4_spinner);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.gradeArray, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		class1Spin.setAdapter(adapter);
+		class2Spin.setAdapter(adapter);
+		class3Spin.setAdapter(adapter);
+		class4Spin.setAdapter(adapter);
+		return fragView;
 	}
-	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setHasOptionsMenu(true);
+		gpaLabel = (TextView) getActivity().findViewById(R.id.gpaLabel);
 	}
-	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		inflater.inflate(R.menu.add, menu);
 	}
@@ -32,6 +48,15 @@ public class FourClassFragment extends Fragment{
 		default:
 			return super.onOptionsItemSelected(item);	
 		}
+	}
+	
+	public void onCalcPress(View view) {
+		final int class1 = class1Spin.getSelectedItemPosition();
+		final int class2 = class2Spin.getSelectedItemPosition();
+		final int class3 = class3Spin.getSelectedItemPosition();
+		final int class4 = class4Spin.getSelectedItemPosition();
+		float GPA = GPACalc.getGPA(class1, class2, class3, class4);
+		gpaLabel.setText(getResources().getText(R.string.gpaLabel) +" "+ Float.toString(GPA));
 	}
 
 }
--- AndroidGPACalculator/src/com/triclops/androidgpacalculator/MainActivity.java
+++ AndroidGPACalculator/src/com/triclops/androidgpacalculator/MainActivity.java
@@ -7,15 +7,9 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity {
 	
-	private Spinner class1Spin, class2Spin, class3Spin, class4Spin;
-	private TextView gpaLabel;
 	private FragmentManager fmanage = getSupportFragmentManager();
 	private FragmentTransaction ftrans = fmanage.beginTransaction();
 
@@ -23,20 +17,9 @@ public class MainActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		gpaLabel = (TextView) findViewById(R.id.gpaLabel);
 		FourClassFragment fragment = new FourClassFragment();
 		ftrans.add(R.id.fragCont, fragment);
 		ftrans.commit();
-		class1Spin = (Spinner) findViewById(R.id.class1_spinner);
-		class2Spin = (Spinner) findViewById(R.id.class2_spinner);
-		class3Spin = (Spinner) findViewById(R.id.class3_spinner);
-		class4Spin = (Spinner) findViewById(R.id.class4_spinner);
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gradeArray, android.R.layout.simple_spinner_item);
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		class1Spin.setAdapter(adapter);
-		class2Spin.setAdapter(adapter);
-		class3Spin.setAdapter(adapter);
-		class4Spin.setAdapter(adapter);
 	}
 
 	@Override
@@ -57,14 +40,6 @@ public class MainActivity extends FragmentActivity {
         }
 }
 	
-	public void onCalcPress(View view) {
-		final int class1 = class1Spin.getSelectedItemPosition();
-		final int class2 = class2Spin.getSelectedItemPosition();
-		final int class3 = class3Spin.getSelectedItemPosition();
-		final int class4 = class4Spin.getSelectedItemPosition();
-		float GPA = GPACalc.getGPA(class1, class2, class3, class4);
-		gpaLabel.setText(getResources().getText(R.string.gpaLabel) +" "+ Float.toString(GPA));
-	}
 	
 
 }
